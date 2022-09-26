9e5040f1e34bb0ed901c614671e88a435f83ba0b
Andrey Shkarin <shkarinandrew@mail.ru>
2022-09-26 21:09:15 +0300

    fix: build

--- util/interface/listCompany.ts
+++ util/interface/listCompany.ts
@@ -6,23 +6,34 @@ export interface ListCompanyData {
 export interface ListCompanyInterface {
   id: string;
   title: string;
-  INN: string[];
-  user: {
-    userName?: string;
-    userProfession?: string;
-  };
-  companyInfo: {
-    NGEE: string[];
-    address: {
-      legalAddress: string;
-      actualAddress: string;
-      streetAddress: string;
-    };
-    OKVED: string;
-    littleName: string;
-    fullName: string;
-    phones?: string;
-    site?: string;
-  };
-  finance: { income?: string; expense?: string };
+  INN: string[] | null;
+  user: User;
+  companyInfo: CompanyInfo;
+  finance: Finance;
+}
+
+export interface CompanyInfo {
+  NGEE: string[];
+  address: Address;
+  OKVED: string;
+  littleName?: string;
+  fullName?: string;
+  phones?: string;
+  site?: string;
+}
+
+export interface Address {
+  legalAddress?: string;
+  actualAddress?: string;
+  streetAddress?: string;
+}
+
+export interface Finance {
+  income?: string;
+  expense?: string;
+}
+
+export interface User {
+  userName?: string;
+  userProfession?: string;
 }
